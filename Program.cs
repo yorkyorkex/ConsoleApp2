18acf811552cf74ea674c5b609a9bf03abc07314
York Yu <n11453559@gmail.com>
2025-09-29 17:59:00 +1000

    Refactor input handling to allow multiple number entries and calculate their sum

--- Program.cs
+++ Program.cs
@@ -2,12 +2,23 @@
 Console.WriteLine("Hello, World!");
 
 
-int number = int.Parse(Console.ReadLine());
-Console.WriteLine($"You entered the number: {number}");
-int number2 = int.Parse(Console.ReadLine());
-Console.WriteLine($"You entered the number2: {number2}");
-int sum = number + number2;
-Console.WriteLine($"The sum is: {sum}");
+// int number = int.Parse(Console.ReadLine());
+// Console.WriteLine($"You entered the number: {number}");
+// int number2 = int.Parse(Console.ReadLine());
+// Console.WriteLine($"You entered the number2: {number2}");
+// int sum = number + number2;
+// Console.WriteLine($"The sum is: {sum}");
+
+
+List<int> numbers = new List<int>();
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Enter number {i + 1}: ");
+    int number = int.Parse(Console.ReadLine());
+    numbers.Add(number);
+}
+int sum = numbers.Sum();
+Console.WriteLine($"The sum of the entered numbers is: {sum}");
 
 
 Console.ReadKey();
