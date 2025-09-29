5f49ea53a889395f9b93aa877cf82c99aa614de4
York Yu <n11453559@gmail.com>
2025-09-29 17:53:05 +1000

    Refactor Program.cs to streamline input handling and output display

--- Program.cs
+++ Program.cs
@@ -1,17 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string name = "Alice";
-int age = 30;
-Console.WriteLine($"Name: {name}, Age: {age}");
-Console.WriteLine(name);
-Console.WriteLine(age);
-
-
-string input = Console.ReadLine();
-Console.WriteLine($"You entered: {input}");
-
-
 
 int number = int.Parse(Console.ReadLine());
 Console.WriteLine($"You entered the number: {number}");
@@ -19,3 +8,6 @@ int number2 = int.Parse(Console.ReadLine());
 Console.WriteLine($"You entered the number2: {number2}");
 int sum = number + number2;
 Console.WriteLine($"The sum is: {sum}");
+
+
+Console.ReadKey();
--- obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
+++ obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a4a964802b301b870d6967588e7593e206656ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
